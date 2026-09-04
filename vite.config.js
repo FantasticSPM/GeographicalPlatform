f301055d5ef5f06f36975a3c458819a912a7fcb6
FantasticSPM <asd507172@gmail.com>
2026-09-04 14:20:36 +0800

    fixed:cesium资源路径优化

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,33 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 // https://vite.dev/config/
 export default defineConfig((e) => {
+
+  const baseUrl = e.mode === "production" ? "/GeographicalPlatform/" : "./"
+  const cesiumSource = "node_modules/cesium/Build/Cesium";
+  const cesiumBaseUrl = "Cesium";
+  const cesiumDir = ['ThirdParty', 'Workers', 'Assets', 'Widgets']
   return {
+    define: {
+      CESIUM_BASE_URL: JSON.stringify(`${baseUrl}${cesiumBaseUrl}`),
+    },
     plugins: [
       vue(),
       vueDevTools(),
+      viteStaticCopy({
+        targets: cesiumDir.map((dir) => ({
+          src: `${cesiumSource}/${dir}`,
+          dest: cesiumBaseUrl,
+          rename: {
+            stripBase: 4
+          }
+        }))
+      })
     ],
-    base: e.mode === "production" ? "/GeographicalPlatform/" : "./",
+    base: baseUrl,
     resolve: {
       alias: {
         '@': fileURLToPath(new URL('./src', import.meta.url)),
