d43a40ccb2cd226fdd4bfd2f6d1bb31142afeba2
FantasticSPM <asd507172@gmail.com>
2026-09-07 23:32:03 +0800

    feat:接入登录、注册接口

---
+++ frontend/src/apis/backend/auth.js
@@ -0,0 +1,19 @@
+import request from "@/utils/request";
+
+// 注册
+export function apiRegister(data) {
+  return request({
+    url: "/auth/register",
+    method: "post",
+    data,
+  });
+}
+
+// 登录
+export function apiLogin(data) {
+  return request({
+    url: "/auth/login",
+    method: "post",
+    data,
+  });
+}
--- frontend/src/utils/request.js
+++ frontend/src/utils/request.js
@@ -3,6 +3,7 @@ import axios from "axios";
 const instance = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   timeout: 10000,
+  withCredentials: true,
 });
 
 // 添加请求拦截器
