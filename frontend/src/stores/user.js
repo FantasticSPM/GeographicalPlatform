29dc55302b6efcca3ea5a9893fbfa8d5d12ccc29
FantasticSPM <asd507172@gmail.com>
2026-09-09 23:09:38 +0800

    feat:user

---
+++ frontend/src/stores/user.js
@@ -0,0 +1,24 @@
+import { ref, computed } from "vue";
+import { defineStore } from "pinia";
+import { apiGetUserInfo } from "@/apis/backend/user";
+import { ElMessage } from "element-plus";
+
+export const useUserStore = defineStore("user", () => {
+  const user = ref(null);
+
+  async function getUser() {
+    const result = await apiGetUserInfo();
+    if (result?.success) {
+      user.value = result.data;
+      return result.data;
+    } else {
+      ElMessage.error(result?.msg || "获取用户信息失败");
+    }
+  }
+
+  function setUser(data) {
+    user.value = data;
+  }
+
+  return { user, setUser, getUser };
+});
