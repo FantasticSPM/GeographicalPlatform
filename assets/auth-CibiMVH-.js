87cb1799fc820f94e8f4620776f44fad36b3e71f
FantasticSPM <FantasticSPM@users.noreply.github.com>
2026-09-09 15:32:30 +0000

    deploy: 578071bf41de1153ad27af53015f42ce5d1c55b2

---
+++ assets/auth-CibiMVH-.js
@@ -0,0 +1,1 @@
+import{n as e}from"./index-BofQSbrx.js";function t(t){return e({url:`/auth/register`,method:`post`,data:t})}function n(t){return e({url:`/auth/login`,method:`post`,data:t})}export{t as n,n as t};
