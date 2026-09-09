a0acc4f1e56d31219fbc87e5c78d3bc24b479472
FantasticSPM <FantasticSPM@users.noreply.github.com>
2026-09-09 17:48:15 +0000

    deploy: 00479faa8515c16165e21f6e96053c27576de0a6

--- assets/auth-sHbAoCso.js
+++
@@ -1,1 +0,0 @@
-import{t as e}from"./request-D9lWEEua.js";function t(t){return e({url:`/auth/register`,method:`post`,data:t})}function n(t){return e({url:`/auth/login`,method:`post`,data:t})}export{t as n,n as t};
