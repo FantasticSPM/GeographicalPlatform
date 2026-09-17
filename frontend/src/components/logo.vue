c249898733c93f0ecf6465b492a705b6aa2ce4c9
FantasticSPM <asd507172@gmail.com>
2026-09-17 17:37:37 +0800

    feat:新增logo组件

---
+++ frontend/src/components/logo.vue
@@ -0,0 +1,23 @@
+<template>
+  <span class="brand__mark">G</span>
+</template>
+
+<script setup></script>
+
+<style scoped lang="scss">
+.brand__mark {
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  width: 35px;
+  height: 35px;
+  border: 2px solid #000;
+  border-radius: 50%;
+  // color: #dff6ff;
+  font-size: 19px;
+  font-weight: 700;
+  line-height: 1;
+  -webkit-text-stroke: 1px rgba(6, 56, 104, 0.38);
+  color: rgba(0, 0, 0, 0.98);
+}
+</style>
